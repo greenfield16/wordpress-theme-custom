8af6f6c7d31a311df02316b0412a8941b40cdd37
Joseph. MD <vuminhdaovmd@gmail.com>
2021-08-17 14:07:13 +0700

    commit 4

--- index.php
+++ index.php
@@ -12,7 +12,13 @@
 
     <header class="main-header">
         <div class="custom-logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+            <?php
+
+            $custom_logo_id = get_theme_mod( 'custom_logo' );
+            $custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );
+            echo '<img src="' . esc_url( $custom_logo_url ) . '" alt="">';
+            ?>
+            
         </div>
         <div class="search"></div>
         <nav class="main-menu"></nav>
