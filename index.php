5baa13282698a679fe61cedb2ab1b0488dd77cd9
Joseph. MD <vuminhdaovmd@gmail.com>
2021-08-17 11:22:20 +0700

    2rd commit

--- index.php
+++ index.php
@@ -7,6 +7,31 @@
     <title>Document</title>
 </head>
 <body>
-    Hello World!
+
+<div id="container">
+
+    <header class="main-header">
+        <div class="custom-logo"></div>
+        <div class="search"></div>
+        <nav class="main-menu"></nav>
+    </header>
+
+    <section class="banner"></section>
+
+    <section class="content-holder">
+        <article class="content"></article>
+        <aside class="sidebar"></aside>
+    </section>
+
+    <footer class="main-footer">
+        <aside class="footer-box"></aside>
+        <aside class="footer-box"></aside>
+        <aside class="footer-box"></aside>
+        <aside class="footer-box"></aside>
+    </footer>
+
+    <div class="powered"></div>
+
+</div>
 </body>
 </html>
