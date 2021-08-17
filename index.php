ce48912efb76651eda7a134aff1822d938fc548f
Joseph. MD <vuminhdaovmd@gmail.com>
2021-08-17 16:12:20 +0700

    commit 5

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
         <article class="content">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <div class="content">
-                <?php if (have_posts() ) : while ( have_posts(  )) : the_post(  ); ?>
+                <?php if (have_posts() ) : while ( have_posts()) : the_post(  ); ?>
                     <div class="article">
                         <h2><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
                         <?php the_excerpt(); ?>
@@ -60,6 +60,7 @@
             <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
         <?php endif; ?>
         </article>
+
         <aside class="sidebar"></aside>
     </section>
 
